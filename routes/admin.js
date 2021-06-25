1e80e6cbfdfc51d27076a76a80db477c1d64abd0
Dariusz Paziewski <paziewski.dariusz@gmail.com>
2021-06-25 07:44:42 +0200

    admin login, admin news

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const News = require('../models/news');
+
 const router = express.Router();
+
+router.all('*', (req, res, next) => {
+  if (!req.session.admin) {
+    return res.redirect('login');
+  }
+
+  next();
+});
 
 /* GET admin page. */
 router.get('/', (req, res) => {
-  res.render('admin', { title: 'Admin' });
+  News.find({}, (err, data) => {
+    console.log('admin/', data);
+    res.render('admin/index', { title: 'Admin', data });
+  });
+});
+
+router.get('/news/add', (req, res) => {
+  res.render('admin/news-form', { title: 'Dodaj news', body: {}, errors: {} });
+});
+
+router.post('/news/add', (req, res) => {
+  const body = req.body;
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  newsData.save(err => {
+    if (err) {
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }
+
+    res.redirect('/admin');
+  });
+});
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, err => {
+    res.redirect('/admin');
+  });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const login = 'admin';
+const password = '1234';
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+/* GET login form. */
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Logowanie' });
+});
+
+/* POST login data. */
+router.post('/login', (req, res) => {
+  const body = req.body;
+
+  if (login === body.login && password === body.password) {
+    req.session.admin = 1;
+    res.redirect('/admin');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
