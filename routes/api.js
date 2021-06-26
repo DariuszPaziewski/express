99a28acc32d89b14b2c431f34d8eaeda0f59e450
Dariusz Paziewski <paziewski.dariusz@gmail.com>
2021-06-26 08:00:17 +0200

    api news

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
+
+const defaultSort = -1;
+
+router.get('/', (req, res) => {
+  const search = req.query.search || '';
+  let sort = req.query.sort || defaultSort;
+
+  if (sort !== 1 || sort !== -1) {
+    sort = defaultSort;
+  }
+
+  const news = News
+    .find({title: new RegExp(search.trim(), 'i')})
+    .sort({created: sort})
+    .select('_id title description');
+
+  news.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const news = News
+      .findById(id)
+      .select('_id title description');
+  
+    news.exec((err, data) => {
+      res.json(data);
+    });
+  });
+
+module.exports = router;
